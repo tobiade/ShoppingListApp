b6532c8bb8ef0b4f0d7f017aee7ca33324d4762f
Sarah-Jessica Jemitola <sarah-jessicajemitola@sarahjesicasmbp.lan>
2016-02-03 22:15:50 +0000

    playing with tableview

--- ShoppingListApp/ViewController.swift
+++ ShoppingListApp/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.textColor=UIColor.redColor()
+        return cell
+    }
 
+    @IBAction func addButton(sender: UIButton) {
+        let newItem = textField.text
+        items.append(newItem!)
+        textField.resignFirstResponder()
+        textField.text=""
+        tableView.reloadData()
+    }
 
 }
 
